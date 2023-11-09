24cfede86cae57ebf1315320d34e9fe8d313a602
bhuvaneshwari-sivasangaran <129428887+bhuvaneshwari-sivasangaran@users.noreply.github.com>
2023-11-09 11:04:59 -0500

    Update 1759_Count_Number_Of_Homogenous_Substrings.js

--- 1759_Count_Number_Of_Homogenous_Substrings.js
+++ 1759_Count_Number_Of_Homogenous_Substrings.js
@@ -10,10 +10,12 @@ var countHomogenous = function(s) {
     for (let i = 1; i < s.length; i++) {
         if (s[i] === s[i - 1]) {
             consecutiveCount++;
-        } else {     count = (count + (consecutiveCount * (consecutiveCount + 1) / 2)) % mod;
+        } else {     
+            count = (count + (consecutiveCount * (consecutiveCount + 1) / 2)) % mod;
             consecutiveCount = 1;
         }
-    }    count = (count + (consecutiveCount * (consecutiveCount + 1) / 2)) % mod;
+    }    
+    count = (count + (consecutiveCount * (consecutiveCount + 1) / 2)) % mod;
 
     return count;
 };
