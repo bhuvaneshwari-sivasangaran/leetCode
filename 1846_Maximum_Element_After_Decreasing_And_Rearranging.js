b123dc1d5cd765b48e68c2976d5e5a5cff13752f
bhuvaneshwari-sivasangaran <129428887+bhuvaneshwari-sivasangaran@users.noreply.github.com>
2023-11-15 08:42:05 -0500

    Create 1846_Maximum_Element_After_Decreasing_And_Rearranging.js

---
+++ 1846_Maximum_Element_After_Decreasing_And_Rearranging.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+   
+   arr.sort((a, b) => a - b);
+    let maxVal = 1;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxVal) {
+            maxVal += 1;
+        }
+    }
+
+    return maxVal;
+
+};
