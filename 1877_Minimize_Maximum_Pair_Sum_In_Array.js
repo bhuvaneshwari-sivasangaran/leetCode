b77d6938e16bfbc95216881d0b1b0e79088c5aa4
bhuvaneshwari-sivasangaran <129428887+bhuvaneshwari-sivasangaran@users.noreply.github.com>
2023-11-17 08:12:42 -0500

    Create 1877_Minimize_Maximum_Pair_Sum_In_Array.js

---
+++ 1877_Minimize_Maximum_Pair_Sum_In_Array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+
+    nums.sort( (a,b) => a-b );
+    let maximum = 0;
+    let start = 0;
+    let end = nums.length - 1;
+
+    while( start < end ){
+        let sum = nums[start] + nums[end];
+        if( maximum < sum ){
+            maximum = sum;
+        }
+        start++;
+        end--;
+    }
+    
+    return maximum;
+
+};
